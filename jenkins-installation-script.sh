1bdd59a14f99e4c346a7932945cbef56648c4dca
maharshi11 <46251835+maharshi11@users.noreply.github.com>
2024-07-12 14:34:38 +0530

    Update jenkins-installation-script.sh

--- jenkins-installation-script.sh
+++ jenkins-installation-script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "installing java"
-#sudo yum install java -y
+sudo yum install java -y
 #sudo amazon-linux-extras install java-openjdk17 -y
-sudo yum install java-17-openjdk -y
+#sudo yum install java-17-openjdk -y
 
 echo "Download the Jenkins repository configuration"
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
